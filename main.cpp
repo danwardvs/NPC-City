297c287f5d2d6db93b9eeb1f027424748e042bb0
danwardvs <danwardvs@adsgames.net>
2014-09-25 14:05:03 -0700

    Added in basic NPC code, first image

--- main.cpp
+++ main.cpp
@@ -2,8 +2,34 @@
 #include<alpng.h>
 
 BITMAP* buffer;
+BITMAP* npc_1;
+
+struct npcs{
+    int x;
+    int y;
+    int gender;
+    int type;
+}npc[10];
+
+//A function to streamline error reporting in file loading
+void abort_on_error(const char *message){
+	 set_window_title("Error!");
+	 if (screen != NULL){
+	    set_gfx_mode(GFX_TEXT, 0, 0, 0, 0);
+	 }
+	 allegro_message("%s.\n %s\n", message, allegro_error);
+	 exit(-1);
+}
+
 
 void update(){
+
+
+
+}
+void draw(){
+    rectfill(buffer,0,0,1024,768,makecol(255,255,255));
+    draw_sprite(buffer,npc_1,npc[1].x,npc[1].y);
     draw_sprite(screen,buffer,0,0);
 
 }
@@ -16,7 +42,14 @@ void update(){
 
 
 void setup(){
-    buffer=create_bitmap(800,600);
+    buffer=create_bitmap(1024,768);
+
+    npc[1].x=100;
+    npc[1].y=200;
+    npc[1].gender=1;
+
+    if (!(npc_1 = load_bitmap("npc_1.png", NULL)))
+      abort_on_error("Cannot find image npc_1.png\nPlease check your files and try again");
 }
 
 
@@ -34,17 +67,18 @@ int main(){
   set_color_depth(32);
 
 
-  set_gfx_mode(GFX_AUTODETECT_WINDOWED, 800,600, 0, 0);
+  set_gfx_mode(GFX_AUTODETECT_WINDOWED, 1024,768, 0, 0);
   install_sound(DIGI_AUTODETECT,MIDI_AUTODETECT,".");
 
 
 
-  set_window_title("Window title here!");
+  set_window_title("NPC City");
   setup();
 
 
     while(!key[KEY_ESC]){
         update();
+        draw();
   	}
 
 	return 0;
