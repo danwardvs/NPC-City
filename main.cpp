f09b5f3a2d7c4d40396d1a28e6cd579dd3462334
danwardvs <danwardvs@adsgames.net>
2014-09-25 14:20:26 -0700

    Added wandering code, priorites

--- main.cpp
+++ main.cpp
@@ -1,15 +1,37 @@
 #include<allegro.h>
 #include<alpng.h>
+#include<time.h>
 
 BITMAP* buffer;
 BITMAP* npc_1;
+BITMAP* vending_machine;
+
+struct vendingmachines{
+    int x;
+    int y;
+}vendingmachine[10];
 
 struct npcs{
     int x;
     int y;
     int gender;
     int type;
+    int speed;
+    int hunger;
+    int money;
+    int thirst;
+    int priority;
 }npc[10];
+
+// Random number generator. Use int random(highest,lowest);
+int random(int newLowest, int newHighest)
+{
+  int lowest = newLowest, highest = newHighest;
+  int range = (highest - lowest) + 1;
+  int randomNumber = lowest+int(range*rand()/(RAND_MAX + 1.0));
+  return randomNumber;
+}
+
 
 //A function to streamline error reporting in file loading
 void abort_on_error(const char *message){
@@ -23,6 +45,25 @@ void abort_on_error(const char *message){
 
 
 void update(){
+    //NPC Parser
+    for(int i=0; i<10; i++){
+
+
+
+    //Wander
+    if(npc[i].priority<1){
+        int randomnum=random(1,npc[i].speed);
+        if(randomnum==1)
+            npc[i].x++;
+        if(randomnum==2)
+            npc[i].y++;
+        if(randomnum==3)
+            npc[i].x--;
+        if(randomnum==4)
+            npc[i].y--;
+
+        }
+    }
 
 
 
@@ -42,14 +83,23 @@ void draw(){
 
 
 void setup(){
+
+    srand(time(NULL));
+
     buffer=create_bitmap(1024,768);
 
     npc[1].x=100;
     npc[1].y=200;
     npc[1].gender=1;
+    npc[1].speed=100;
+    npc[1].hunger=25;
+    npc[1].money=100;
+    npc[1].thirst=100;
 
     if (!(npc_1 = load_bitmap("npc_1.png", NULL)))
       abort_on_error("Cannot find image npc_1.png\nPlease check your files and try again");
+    if (!(vending_machine = load_bitmap("vending_machine.png", NULL)))
+      abort_on_error("Cannot find image vending_machine.png\nPlease check your files and try again");
 }
 
 
