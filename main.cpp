cbdee11ca9b118b9b13c100805eda1160597981c
danwardvs <danwardvs@adsgames.net>
2014-09-25 18:44:39 -0700

    Gets thirsty, runs to machine

--- main.cpp
+++ main.cpp
@@ -8,6 +8,32 @@ BITMAP* buffer;
 BITMAP* npc_1;
 BITMAP* vending_machine;
 
+bool close_button_pressed;
+
+// FPS System
+volatile int ticks = 0;
+const int updates_per_second = 60;
+volatile int game_time = 0;
+
+int fps;
+int frames_done;
+int old_time;
+
+void ticker(){
+  ticks++;
+}
+END_OF_FUNCTION(ticker)
+
+void game_time_ticker(){
+  game_time++;
+}
+END_OF_FUNCTION(ticker)
+
+void close_button_handler(void){
+  close_button_pressed = TRUE;
+}
+END_OF_FUNCTION(close_button_handler)
+
 struct vendingmachines{
     int x;
     int y;
@@ -21,7 +47,7 @@ struct npcs{
     int speed;
     int hunger;
     int money;
-    int thirst;
+    float thirst;
     int priority;
 }npc[10];
 
@@ -54,7 +80,7 @@ void abort_on_error(const char *message){
 void update(){
     //NPC Parser
     for(int i=0; i<10; i++){
-
+        npc[i].thirst-=0.1;
         //Get a drink
         if(npc[i].thirst<75){
             if(npc[i].priority<1)
@@ -69,10 +95,17 @@ void update(){
                 if(distance_to_object(npc[i].x,npc[i].y,vendingmachine[j].x,vendingmachine[j].y)<distance_will_travel)
                     closest_vending_machine=j;
             }
-            if(vendingmachine[closest_vending_machine].x>npc[i].x)npc[i].x+=1;
-            if(vendingmachine[closest_vending_machine].x<npc[i].x)npc[i].x-=1;
-            if(vendingmachine[closest_vending_machine].y>npc[i].y)npc[i].y+=1;
-            if(vendingmachine[closest_vending_machine].y<npc[i].y)npc[i].y-=1;
+            if(vendingmachine[closest_vending_machine].x+15>npc[i].x)npc[i].x+=1*npc[i].speed;
+            if(vendingmachine[closest_vending_machine].x+15<npc[i].x)npc[i].x-=1*npc[i].speed;
+            if(vendingmachine[closest_vending_machine].y+30>npc[i].y)npc[i].y+=1*npc[i].speed;
+            if(vendingmachine[closest_vending_machine].y+30<npc[i].y)npc[i].y-=1*npc[i].speed;
+            if(vendingmachine[closest_vending_machine].y+30==npc[i].y && vendingmachine[closest_vending_machine].x+15==npc[i].x){
+                if(npc[i].money>2){
+                    npc[i].thirst+=50;
+                    if(npc[i].thirst>75)npc[i].priority=0;
+                    npc[i].money-=2;
+                }
+            }
         }
 
         //Wander
@@ -82,10 +115,10 @@ void update(){
                 npc[i].x++;
             if(randomnum==2)
                 npc[i].y++;
-            if(randomnum==3)
-                npc[i].x--;
-            if(randomnum==4)
-                npc[i].y--;
+            //if(randomnum==3)
+               // npc[i].x--;
+           // if(randomnum==4)
+                //npc[i].y--;
 
             }
         }
@@ -93,12 +126,15 @@ void update(){
 
 
 }
-void draw(){
+void draw(bool to_screen){
     rectfill(buffer,0,0,1024,768,makecol(255,255,255));
     for(int i=0; i<10; i++){
         draw_sprite(buffer,vending_machine,vendingmachine[i].x,vendingmachine[i].y);
     }
     draw_sprite(buffer,npc_1,npc[1].x,npc[1].y);
+    textprintf_centre_ex( buffer, font, npc[1].x+16, npc[1].y-10, makecol(0,0,0), -1, "%i", npc[1].money);
+    textprintf_centre_ex( buffer, font, npc[1].x+16, npc[1].y-18, makecol(0,0,0), -1, "%4.2f", npc[1].thirst);
+    textprintf_centre_ex( buffer, font, npc[1].x+16, npc[1].y-26, makecol(0,0,0), -1, "%i", npc[1].priority);
     draw_sprite(screen,buffer,0,0);
 
 }
@@ -114,12 +150,25 @@ void setup(){
 
     srand(time(NULL));
 
+     // Setup for FPS system
+    LOCK_VARIABLE(ticks);
+    LOCK_FUNCTION(ticker);
+    install_int_ex(ticker, BPS_TO_TIMER(updates_per_second));
+
+    LOCK_VARIABLE(game_time);
+    LOCK_FUNCTION(game_time_ticker);
+    install_int_ex(game_time_ticker, BPS_TO_TIMER(10));
+
+    // Close button
+    LOCK_FUNCTION(close_button_handler);
+    set_close_button_callback(close_button_handler);
+
     buffer=create_bitmap(1024,768);
 
     npc[1].x=500;
     npc[1].y=100;
     npc[1].gender=1;
-    npc[1].speed=100;
+    npc[1].speed=5;
     npc[1].hunger=25;
     npc[1].money=100;
     npc[1].thirst=20;
@@ -157,10 +206,28 @@ int main(){
   setup();
 
 
-    while(!key[KEY_ESC]){
+    while(!key[KEY_ESC] && !close_button_pressed){
+        while(ticks == 0){
+            rest(1);
+        }
+    while(ticks > 0){
+        int old_ticks = ticks;
+
         update();
-        draw();
-  	}
+
+        ticks--;
+        if(old_ticks <= ticks){
+            break;
+        }
+    }
+        if(game_time - old_time >= 10){
+            fps = frames_done;
+            frames_done = 0;
+            old_time = game_time;
+        }
+        draw(true);
+    }
+
 
 	return 0;
 }
